7b6ab84a2fd1d144a8c29ec534cf7e54082a4215
pediredlaramu0704-hue <pediredlaramu0704@gmail.com>
2026-03-12 21:42:12 +0530

    Add caching functionality to service worker
    Implement caching for assets in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,24 @@
-self.addEventListener("install", function(event) {
-  console.log("Service Worker Installed");
-});
+const CACHE_NAME = "love-app-v1"
 
-self.addEventListener("fetch", function(event) {});
+const urlsToCache = [
+  "index.html",
+  "photo1.jpeg",
+  "photo2.jpeg",
+  "photo3.jpeg",
+  "music.mp3",
+  "icon.png"
+]
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  )
+})
