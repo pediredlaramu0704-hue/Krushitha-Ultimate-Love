9161af0529ef03f807b6d8aab84cbab8f40eb779
pediredlaramu0704-hue <pediredlaramu0704@gmail.com>
2026-03-12 22:04:43 +0530

    Rename cache and update cached URLs in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,26 @@
-const CACHE_NAME = "love-story-v1"
+const CACHE_NAME = "love-app"
 
 const urlsToCache = [
-  "index.html",
-  "manifest.json",
-  "icon.png",
-  "music.mp3",
-  "photo1.jpeg",
-  "photo2.jpeg",
-  "photo3.jpeg"
+  "/",
+  "/index.html",
+  "/icon.png",
+  "/photo1.jpeg",
+  "/photo2.jpeg",
+  "/photo3.jpeg"
 ]
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache)
+    })
   )
 })
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-    .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+    })
   )
 })
