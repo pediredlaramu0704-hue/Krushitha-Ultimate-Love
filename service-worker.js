29e4cfa1dbbf10fb7f3403ef0bc67e8f3511ac74
pediredlaramu0704-hue <pediredlaramu0704@gmail.com>
2026-03-12 21:56:56 +0530

    Rename cache version and update cached files

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,25 @@
-const CACHE_NAME = "love-app-v1"
+const CACHE_NAME = "love-story-v1"
 
 const urlsToCache = [
   "index.html",
+  "manifest.json",
+  "icon.png",
+  "music.mp3",
   "photo1.jpeg",
   "photo2.jpeg",
-  "photo3.jpeg",
-  "music.mp3",
-  "icon.png"
+  "photo3.jpeg"
 ]
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    .then(cache => cache.addAll(urlsToCache))
   )
 })
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    .then(response => response || fetch(event.request))
   )
 })
